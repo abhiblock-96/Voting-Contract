b41f7f631fb02bdbd6fc8aa3c95f1c0b083cf75b
Abhishek Maurya <abhiofficialwork96@gmail.com>
2026-07-15 03:31:09 +0000

    fix: use correct forge-std console import

--- test/unit/VotingContractUnitTest.t.sol
+++ test/unit/VotingContractUnitTest.t.sol
@@ -1,7 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import {console} from "forge-std/Console.sol";
 import "src/VotingContract.sol";
 import {BaseTest} from "test/BaseTest.t.sol";
 
